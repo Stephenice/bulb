7f7f123008f7fb22ffdb0b3e50cc93f283d3f393
Stephenice <stephencode@hotmail.com>
2021-01-28 23:59:08 +0100

    bulb

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const onOffSwitch = document.querySelector('.onoffswitch-inner');
-const onSwitch = getComputedStyle(onOffSwitch, '::after');
-const offSwitch = getComputedStyle(onOffSwitch, '::before');
+
 const bulb= document.querySelector('#myImage');
 
 
 
 onOffSwitch.addEventListener('click', function(){
-
     if(bulb.src.match('pic_bulbon')){
         bulb.src='pic_bulboff.gif';
-        console.log(offSwitch.content);
+       // console.log(offSwitch.content);
     }else{
         bulb.src='pic_bulbon.gif';
+       // console.log(onSwitch.content);
     }
 })
