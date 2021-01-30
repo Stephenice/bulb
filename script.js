c059a5812c6219aae94e27113af90686b57cd94a
Stephenice <stephencode@hotmail.com>
2021-01-30 05:21:57 +0100

    cal files

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const onOffSwitch = document.querySelector('.onoffswitch-inner');
-
 const bulb= document.querySelector('#myImage');
 
 
@@ -9,9 +8,7 @@ const bulb= document.querySelector('#myImage');
 onOffSwitch.addEventListener('click', function(){
     if(bulb.src.match('pic_bulbon')){
         bulb.src='pic_bulboff.gif';
-       // console.log(offSwitch.content);
     }else{
         bulb.src='pic_bulbon.gif';
-       // console.log(onSwitch.content);
     }
 })
